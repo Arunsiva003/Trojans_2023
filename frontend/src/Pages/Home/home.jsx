3e3b842e25cf5acbe9009e8159ebeecc4567105e
ArunSiva <Arunsiva003@users.noreply.github.com>
2023-09-01 22:02:32 +0530

    home

--- frontend/src/Pages/Home/home.jsx
+++ frontend/src/Pages/Home/home.jsx
@@ -369,11 +369,15 @@ export default function Home() {
 
   return (
     <div className='homePage'>
+
+
         <div className='intro'>
             <p className='trojanshead'>TROJANS</p> 
             <img src={Logo} alt="logo" className='homeLogo'/>
             <div className='line'></div>
         </div>
+
+
         <div className='hero'>
           <div className="heroTrojans">TROJANS’23</div>
           <div className="heroDesc">National Level Technical Symposium</div>
@@ -381,23 +385,26 @@ export default function Home() {
               <Timer/>
           </div>
           <div className="heroDate">September 20, 2023</div>
-          <div className="registerBox">
+          {/* <div className="registerBox">
               <div className="registerButton">REGISTER</div>
-          </div>
+          </div> */}
+        
           <Scroller/>
         </div>
+
+
         <div className='homeAbout'>
           <div className='aboutHead'><div className='aboutHead2'>About Us</div></div>
            <div className='aboutContainer'>
               <div className='firstAbout'>
                   <div className='leftAbout'>
-                    <img src={citLogo} alt="cit"/>
+                    <img src={citLogo} alt="cit" className='aboutLogo'/>
                   </div>
                   <div className='rightAbout'>
                     <p>{cit}</p>
                   </div>
               </div>
-              <div className='firstAbout'>
+              <div className='firstAbout2'>
                   <div className='leftAbout'>  
                     <p>{cit}</p>
                   </div>
@@ -408,6 +415,9 @@ export default function Home() {
            </div>
           <div className='line'></div>
         </div>
+
+
+
         <div className='eventsHome'>
           <div className='eventHead'>
             <div className='eventHead2'>Events</div>
