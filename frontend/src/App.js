c82cdffe173386a9bb29c0ba5012ed5258926281
Sameerul-Hak <sameerulhakofficial@gmail.com>
2023-09-04 19:18:30 +0530

    abc

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,36 @@
-import Home from "./Pages/Home/home";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import EventTemp from "./components/EventsTemp/EventTemp";
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
+import Home from './Pages/Home/home';
+import EventTemp from './components/EventsTemp/EventTemp';
+import Sponsors from './components/Sponsors/Sponsors';
+import DetailEvent from './components/EventDetailtemp/DetailEvent';
+import Workshop from './components/Workshop/Workshop';
+import WorkDetail from './components/WorkDetail/WorkDetail';
 
-import Sponsors from "./components/Sponsors/Sponsors";
+function ScrollToTop() {
+  const { pathname } = useLocation();
 
-import DetailEvent from "./components/EventDetailtemp/DetailEvent";
-import Workshop from "./components/Workshop/Workshop";
-import WorkDetail from "./components/WorkDetail/WorkDetail";
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
 
 function App() {
   return (
-        <Router>
-        {/* <Navbar/> */}
-        <div>
-          <Routes>
-            <Route path="/" element={<Home/>} />
-            <Route path="/sponsors" element={<Sponsors />} />
-            {/* <Route path="/online" element={<Online />} />
-            <Route path="/register" element={<Register />} /> */}
-            {/* <Route path="/events" element={<EventSelect />} /> */}
-            <Route path="/events/:eventName" element={<EventTemp />} />
-            {/* <Route path="/nontech" element={<Nontech />} /> */}
-            {/* <Route path="/contact" element={<ContactUs />} />
-            {/* <Route path="/eventdetail" element={<DetailEvent />} /> */}
-            <Route path="/workshop" element={<Workshop />} />
-            <Route path= "/workdetail" element={<WorkDetail/>} />
-          <Route path= "/detail/:eventtype/:name" element={<DetailEvent/>} />
-          </Routes>
-        </div>
+    <Router>
+      <ScrollToTop />
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sponsors" element={<Sponsors />} />
+          <Route path="/events/:eventName" element={<EventTemp />} />
+          <Route path="/workshop" element={<Workshop />} />
+          <Route path="/workdetail" element={<WorkDetail />} />
+          <Route path="/detail/:eventtype/:name" element={<DetailEvent />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
